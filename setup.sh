55fcac0fa803e08dca15accfc0b9d2a4ea8c3a51
Kaoru Nagashima <nkchan0228@gmail.com>
2023-01-22 14:02:25 +0900

    check implamentation apt and git confgi

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 apt update -y
-apt install ansible libselinux-python git vim
+apt install ansible libselinux-python git vim sshpass ssh
 ansible --version
+systemctl start ssh
 git clone git@github.com:nkchan/dev-env-playbook.git
